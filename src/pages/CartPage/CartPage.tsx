ebcef64ce5df63b2538a990a3e5a8c8390ac8322
StefanGorgevik <stefangorgevik@hotmail.com>
2023-12-19 18:48:59 +0100

    Cart page ui fixes

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -15,7 +15,7 @@ export const CartPage: FC = () => {
   if (cart.length === 0) {
     return (
       <NoContentFound>
-        <Box sx={{ display: "flex", flexDirection: "column", marginTop: 25}}>
+        <Box sx={{ display: "flex", flexDirection: "column", marginTop: 25 }}>
           <Typography color="primary" variant="overline">
             You have no cart items. Please add some.
           </Typography>
@@ -33,18 +33,27 @@ export const CartPage: FC = () => {
   }
 
   return (
-    <Container>
-      <Grid
-        container
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "90vh",
+        justifyContent: "space-between",
+        paddingBottom: 5,
+      }}
+    >
+      <Box
         sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
           gap: 2,
-          height: 500,
+          height: "90%",
           overflowY: "auto",
-          justifyContent: "center",
         }}
       >
         {cart && cart.map((id) => <CartCard key={id} id={id} />)}
-      </Grid>
+      </Box>
 
       <Grid
         item
@@ -53,7 +62,7 @@ export const CartPage: FC = () => {
           flexDirection: "row",
           justifyContent: "flex-end",
           gap: 2,
-          paddingRight: 10,
+          paddingRight: 2,
           marginTop: 2,
         }}
       >
